6cee077db80d35941684f4cb567e462a43efdc72
rasheedarahimi <rasheedarahimi10@outlook.com>
2023-02-19 14:55:49 +0000

    initial commit

--- Bag.js
+++ Bag.js
@@ -2,7 +2,17 @@ class Bag {
     // Include constructor Method with the following property: 
         // 1. Weight
             //1a. Should handle an exception in case user adds no weight and throw an error.
-  
+    constructor(weight){
+        if(!weight){
+            throw new Error("bag needs weight") 
+        }
+        this.weight = weight;
+    }
 }
-
+try {
+    const bag = new Bag(12);
+}catch(error){
+    console.log(error);
+}
 //Export the module
+module.exports = Bag;
--- Person.js
+++ Person.js
@@ -5,6 +5,13 @@ class Person {
             // 2a. Bags should contain/reference a data type for storing more than one bag object.
 
     // Include an addBag method
+    constructor(name){
+        this.name = name;
+    }
+    constructor(bags){
+        this.bags = bags;
+    }
 }
 
 //Export the Module
+module.exports = Person;
